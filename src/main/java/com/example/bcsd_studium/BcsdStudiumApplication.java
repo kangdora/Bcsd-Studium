120c6484784a1cbc67fbe7cc65586f52f5f04ea4
KangDora <ioprt123@gmail.com>
2025-07-07 05:04:19 +0900

    init settings

---
+++ src/main/java/com/example/bcsd_studium/BcsdStudiumApplication.java
@@ -0,0 +1,13 @@
+package com.example.bcsd_studium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcsdStudiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcsdStudiumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bcsd_studium/BcsdStudiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bcsd_studium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcsdStudiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
