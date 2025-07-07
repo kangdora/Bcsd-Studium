fc11f57f661f0c4db3863039dfb436a374154867
KangDora <ioprt123@gmail.com>
2025-07-07 19:02:46 +0900

    fix: 빠져있는 엔드포인트 추가

--- src/main/java/com/example/bcsd_studium/controller/UserController.java
+++ src/main/java/com/example/bcsd_studium/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.example.bcsd_studium.controller;
 
+import com.example.bcsd_studium.dto.UserRankDto;
 import com.example.bcsd_studium.dto.UserResponseDto;
 import com.example.bcsd_studium.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -16,5 +19,10 @@ public class UserController {
     @GetMapping("/{loginId}")
     public ResponseEntity<UserResponseDto> getUser(@PathVariable String loginId) {
         return ResponseEntity.ok(userService.getUserByLoginId(loginId));
+    }
+
+    @GetMapping("/rank")
+    public ResponseEntity<List<UserRankDto>> getUserRank() {
+        return ResponseEntity.ok(userService.getUserRankings());
     }
 }
