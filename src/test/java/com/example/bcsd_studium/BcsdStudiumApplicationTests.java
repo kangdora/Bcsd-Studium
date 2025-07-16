2a48aace2f2b13781da9fa364824d2643b53ad33
KangDora <ioprt123@gmail.com>
2025-07-17 00:36:04 +0900

    test: 테스팅 코드 작성

--- src/test/java/com/example/bcsd_studium/BcsdStudiumApplicationTests.java
+++ src/test/java/com/example/bcsd_studium/BcsdStudiumApplicationTests.java
@@ -2,9 +2,26 @@ package com.example.bcsd_studium;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import com.example.bcsd_studium.domain.repository.UserRepository;
+import com.example.bcsd_studium.domain.repository.ExamRepository;
+import com.example.bcsd_studium.domain.repository.AnswerRepository;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration",
+		"jwt.secret=01234567890123456789012345678901",
+		"jwt.expiration=3600"
+})
 class BcsdStudiumApplicationTests {
+
+	@MockitoBean
+	private UserRepository userRepository;
+
+	@MockitoBean
+	private ExamRepository examRepository;
+
+	@MockitoBean
+	private AnswerRepository answerRepository;
 
 	@Test
 	void contextLoads() {
